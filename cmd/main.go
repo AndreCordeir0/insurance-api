f90b64ee7c3231c217d525f788ca7621d21bf751
Andre Cordeiro <andrezaocordeiro@gmail.com>
2023-12-04 22:51:51 -0300

    config: Adicionando docker-compose

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World"))
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+
+	fmt.Println("Escutando na porta :8080...")
+	err := http.ListenAndServe(":8080", nil)
+
+	if err != nil {
+		panic(err)
+	}
 }
---
+++ dockerfile
@@ -0,0 +1,14 @@
+FROM golang:1.16-alpine
+
+WORKDIR "/app/insurance-api"
+
+COPY go.mod ./
+
+RUN go mod download
+
+COPY . .
+
+RUN go build -o insurance-api cmd/main.go
+
+
+CMD [ "./insurance-api" ]
